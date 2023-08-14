fa00500d99290f26eba0e81239a4e1adb53c48b3
Karpik <artem19051664@gmail.com>
2023-08-14 23:02:44 +0300

    Add constructor to ReferenceData with single parameter (Type)

--- Attribute/Editor/Parameters/ReferenceData.cs
+++ Attribute/Editor/Parameters/ReferenceData.cs
@@ -1,14 +1,11 @@
 using Paulsams.MicsUtils;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using UnityEngine;
 
 namespace ChoiceReferenceEditor
 {
-
     public class ReferenceData
     {
         private const string _nullableNameInPopup = "None";
@@ -20,19 +17,25 @@ namespace ChoiceReferenceEditor
         public readonly ChoiceReferenceAttribute Attribute;
         public readonly int IndexNullVariable;
 
-        public ReferenceData(Type fieldType, ChoiceReferenceAttribute choiceReferenceAttribute)
+        public ReferenceData(Type fieldType, ChoiceReferenceAttribute choiceReferenceAttribute) : this(fieldType)
+        {
+            List<string> typesNames = Types.Select(type => type.Name).ToList();
+            if (choiceReferenceAttribute.Nullable)
+                typesNames.Insert(0, _nullableNameInPopup);
+
+            TypesNames = typesNames.ToArray();
+            Attribute = choiceReferenceAttribute;
+            IndexNullVariable = choiceReferenceAttribute.Nullable ? 0 : -1;
+        }
+
+        public ReferenceData(Type fieldType)
         {
             Type typeProperty = ReflectionUtilities.GetArrayOrListElementTypeOrThisType(fieldType);
             Types = ReflectionUtilities.GetFinalAssignableTypesFromAllTypes(typeProperty);
             List<string> typesNames = Types.Select(type => type.Name).ToList();
 
-            if (choiceReferenceAttribute.Nullable)
-                typesNames.Insert(0, _nullableNameInPopup);
-
             TypesNames = typesNames.ToArray();
-
-            Attribute = choiceReferenceAttribute;
-            IndexNullVariable = choiceReferenceAttribute.Nullable ? 0 : -1;
+            IndexNullVariable = -1;
         }
     }
 }
